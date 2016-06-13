5d137989022fb042ba48c01ef92a27a9a0a23ca6
takashi mori <tmori@esm.co.jp>
2016-06-13 18:25:50 +0900

    ツール自動化を促進した．

--- trunk/build/tools/create_debuginfo.sh
+++ trunk/build/tools/create_debuginfo.sh
@@ -11,7 +11,21 @@ DUMP_FILE=${1}
 cp ${DUMP_FILE} ../asm.dump
 
 cd ../gui_debugger
+
+cp DebuggerTemplate.xlsx Debugger.xlsx
 ruby debugger_v850.rb init
 cp symbol.c ../
+
+
+SRC_FILE="asm.csv"
+SRC_SHEET="asm"
+SRC_RANGE="a3:d16000"
+
+DST_FILE="Debugger.xlsx"
+DST_SHEET="asm"
+DST_RANGE="b5"
+
+ruby excel_range_copy.rb  ${SRC_FILE} ${SRC_SHEET} ${SRC_RANGE} ${DST_FILE} ${DST_SHEET} ${DST_RANGE}
+
 cd ../tools
 

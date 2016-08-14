0bebb19ffeb005640b5dab15e200a1f73a916565
takashi mori <tmori@esm.co.jp>
2016-08-14 18:09:50 +0900

    workをコミット

---
+++ trunk/os/asp/DEMO/can_driver_cfg.h
@@ -0,0 +1,9 @@
+#ifndef _CAN_DRIVER_CFG_H_
+#define _CAN_DRIVER_CFG_H_
+
+#define INHNO_CAN 	69		
+#define INTNO_CAN 	69	 	
+#define INTPRI_CAN	(-5)	
+#define INTATR_CAN	TA_NULL	
+
+#endif /* _CAN_DRIVER_CFG_H_ */

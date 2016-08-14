332d389a147a240287a98fa746331054506c68eb
takashi mori <tmori@esm.co.jp>
2016-08-15 05:57:18 +0900

    CAN割込み番号を変更した．

--- trunk/os/asp/DEMO/can_driver_cfg.h
+++ trunk/os/asp/DEMO/can_driver_cfg.h
@@ -1,8 +1,8 @@
 #ifndef _CAN_DRIVER_CFG_H_
 #define _CAN_DRIVER_CFG_H_
 
-#define INHNO_CAN 	69		
-#define INTNO_CAN 	69	 	
+#define INHNO_CAN 	77		
+#define INTNO_CAN 	77	 	
 #define INTPRI_CAN	(-5)	
 #define INTATR_CAN	TA_NULL	
 

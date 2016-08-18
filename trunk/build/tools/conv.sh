4213a2ef38ba2e7bce22332e233487c2f1cdc6fd
takashi mori <tmori@esm.co.jp>
2016-08-18 10:09:58 +0900

    自動化対応漏れを修正した．

--- trunk/build/tools/conv.sh
+++ trunk/build/tools/conv.sh
@@ -12,6 +12,8 @@ v850-elf-objcopy.exe -O binary ${ELF} ${ELF}.bin
 #v850-elf-objdump.exe -S -d ${ELF} > ${ELF}.dump
 v850-elf-objdump.exe -l -D ${ELF} > ${ELF}.dump
 
+sh elf2symbol.sh ${ELF}
+
 sh create_debuginfo.sh ${ELF}.dump
 
 cd ..

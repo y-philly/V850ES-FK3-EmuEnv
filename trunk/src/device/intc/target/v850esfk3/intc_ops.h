e3e44e3280023d7f556bc48f4fbb4b7627a1cad5
takashi mori <tmori@esm.co.jp>
2016-12-04 08:00:22 +0900

    割込みコントローラを追加した

---
+++ trunk/src/device/intc/target/v850esfk3/intc_ops.h
@@ -0,0 +1,11 @@
+#ifndef _INTC_OPS_H_
+#define _INTC_OPS_H_
+
+extern void intc_init(CpuManagerType *cpu);
+extern int intc_raise_intr(CpuManagerType *cpu, uint32 intno);
+extern bool intc_has_pending_intr(CpuManagerType *cpu);
+extern int intc_raise_pending_intr(CpuManagerType *cpu);
+extern void intc_clr_nmi(CpuManagerType *cpu);
+extern void intc_clr_currlvl_ispr(CpuManagerType *cpu);
+
+#endif /* _INTC_OPS_H_ */
---
+++ trunk/src/lib/dbg_print.c
@@ -0,0 +1,58 @@
+#include "dbg.h"
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+DbgExecOpBufferType DbgExecOpBuffer;
+
+void dbg_print_init(char *filepath)
+{
+	int fd;
+	fd = open(filepath, O_CREAT | O_TRUNC |O_WRONLY | O_BINARY, 0777);
+	if (fd < 0) {
+		printf("debugger_exec_op_bufinit:open err=%d\n", errno);
+		fflush(stdout);
+		exit(1);
+	}
+	DbgExecOpBuffer.is_view_mode = FALSE;
+	DbgExecOpBuffer.fd = fd;
+	DbgExecOpBuffer.filepath = filepath;
+	DbgExecOpBuffer.count = 0;
+	return;
+}
+
+void dbg_print_sync(void)
+{
+	int i;
+	int err;
+
+	for (i = 0; i < DbgExecOpBuffer.count; i++) {
+		err = write(DbgExecOpBuffer.fd, DbgExecOpBuffer.buf[i].p, DbgExecOpBuffer.buf[i].write_len);
+		if (err <= 0) {
+			printf("debugger_exec_op_bufsync:write err=%d\n", errno);
+			fflush(stdout);
+			exit(1);
+		}
+	}
+	err = close(DbgExecOpBuffer.fd);
+	if (err < 0) {
+		printf("debugger_exec_op_bufsync:close err=%d\n", errno);
+		fflush(stdout);
+		exit(1);
+	}
+	int fd;
+	fd = open(DbgExecOpBuffer.filepath, O_APPEND |O_WRONLY | O_BINARY);
+	if (fd < 0) {
+		printf("debugger_exec_op_bufsync:open err=%d\n", errno);
+		fflush(stdout);
+		exit(1);
+	}
+	DbgExecOpBuffer.fd = fd;
+	DbgExecOpBuffer.count = 0;
+	return;
+}
+
+

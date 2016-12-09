9f91839057989024cfd8b6cc54ff599952a6b5ec
takashi mori <tmori@esm.co.jp>
2016-12-09 16:51:29 +0900

    ELFローダーを追加した．

--- trunk/src/main/main.c
+++ trunk/src/main/main.c
@@ -1,8 +1,20 @@
 #include "front/parser/dbg_parser.h"
 #include "front/parser/lib/dbg_parser_lib.h"
+#include "loader/loader.h"
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
 
+#define DBG_LOADER_TEST
+
+#ifdef DBG_LOADER_TEST
+#define BUFSIZE (1024*1024)
+static uint8 buf[BUFSIZE];
+#endif
+
+#ifdef DBG_PARSER_TEST
 static int debugger_getline(char *line, int size)
 {
 	int n = 0;
@@ -18,12 +30,11 @@ static int debugger_getline(char *line, int size)
 	return n;
 }
 
-int main(int argc, const char *argv[])
+static void dbg_parser_test(void)
 {
 	DbgCmdExecutorType *res;
 	char buffer[1024];
 	int len;
-
 
 	while (TRUE) {
 		printf(">");
@@ -36,6 +47,50 @@ int main(int argc, const char *argv[])
 			res->run(res);
 		}
 	}
+}
+#endif
 
+#ifdef DBG_LOADER_TEST
+static int dbg_loader_test(int argc, const char *argv[])
+{
+    int len = 0;
+    int ret = 0;
+    int fd;
+    FILE *fp;
+    struct stat st;
+	Std_ReturnType err;
+
+    if (argc != 2) {
+		fprintf(stderr, "argc=%d\n", argc);
+		fprintf(stderr, "Usage: elf\n");
+		return 1;
+    }
+    fp = fopen(argv[1], "rb");
+    if (fp == NULL) {
+		fprintf(stderr, "ERROR %s\n", argv[1]);
+		fprintf(stderr, "Usage: elf\n");
+		return 1;
+    }
+    fd = fileno(fp);
+
+    fstat(fd, &st);
+    printf("st.st_size=%d\n", st.st_size);
+    len = fread(buf, st.st_size, 1, fp);
+    printf("len=%d\n", len);
+
+	err = elf_load(buf);
+
+    fclose(fp);
+}
+#endif
+
+int main(int argc, const char *argv[])
+{
+#ifdef DBG_PARSER_TEST
+	dbg_parser_test();
 	return 0;
+#endif
+#ifdef DBG_LOADER_TEST
+	return dbg_loader_test(argc, argv);
+#endif
 }

030e3cf9421e71c4816aa07cafeb5c61087e07de
takashi mori <tmori@esm.co.jp>
2017-01-07 08:50:58 +0900

    コードテンプレート部分を追加した．

---
+++ trunk/src/test/tool/TestCodeGenerator/TestCodeGenerator.rb
@@ -0,0 +1,70 @@
+#! ruby -Ks
+require 'erb'
+require 'csv'
+require 'fileutils'
+require 'TestObjectCreator.rb'
+
+class TemplateFileMap
+    attr_accessor :output
+    attr_accessor :template
+
+    def initialize(line)
+  self.output   = line[0].strip
+  self.template   = line[1].strip
+    end
+end
+
+class TemplateFileParser
+  def initialize(file)
+    @filepath = file
+    @list = Array.new()
+  end
+
+  def parse()
+    CSV.foreach(@filepath) do |line|
+      @list[@list.length] = TemplateFileMap.new(line)
+    end
+  end
+
+  def get()
+    return @list
+  end
+end
+
+def render path
+  src = ERB.new(File.read(path), nil, '-').result(binding)
+end
+
+def main(test_item_file, csv_file)
+  creator = TestObjectCreator.new(test_item_file)
+  spec = creator.load()
+  
+  parser = TemplateFileParser.new(csv_file)
+  parser.parse
+
+  #generate for each template
+  parser.get.each { |entry|
+    src = render(entry.template)
+    open(entry.output, "w") { |f|
+      f.write(src)
+      puts "### Created: " + entry.output
+    }
+  }
+end
+
+if ARGV.length != 2
+  printf("Usage: TestCodeGenerator.rb test_item_file template.csv")
+  exit(1)
+end
+
+test_item_file=ARGV[0]
+csv_file=ARGV[1]
+
+begin
+  main(test_item_file, csv_file)
+rescue => e
+  p e.message
+  exit(1)
+end
+
+exit(0)

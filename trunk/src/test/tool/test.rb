c9aecad78d85d358abc8935b88e0011aa2572097
takashi mori <tmori@esm.co.jp>
2017-01-06 18:22:29 +0900

    テストオブジェクトの作成完了

--- trunk/src/test/tool/test.rb
+++ trunk/src/test/tool/test.rb
@@ -5,6 +5,9 @@ require 'TestTargetFunc.rb'
 require 'TestTargetFuncInput.rb'
 require 'TestTargetFuncOutput.rb'
 require 'TestSpec.rb'
+require 'TestItem.rb'
+require 'TestTargetFuncInputValue.rb'
+require 'TestTargetFuncExpectedValue.rb'
 
 target = TestTargetFunc.new("add")
 
@@ -22,5 +25,35 @@ target.addOutput(TestTargetFuncOutput.new(cls2.new("psw"), cls1.new("r23"), cls1
 spec = TestSpec.new("ADD1")
 spec.setTarget(target)
 
-p spec.ins()
+item = TestItem.new(spec.name + "_1")
+item.addInput(TestTargetFuncInputValue.new("0x2"))
+item.addInput(TestTargetFuncInputValue.new("0x7FFFFFFE"))
 
+item.addOutput(TestTargetFuncExpectedValue.new("0x80000000"))
+item.addOutput(TestTargetFuncExpectedValue.new("0x6"))
+
+spec.addItem(item)
+
+
+
+print "/* set input */\n"
+print "mov " + item.inp[0].value + ", " + target.inp[0].elm.name + "\n"
+print "mov " + item.inp[1].value + ", " + target.inp[1].elm.name + "\n"
+
+print "/* set expect */\n"
+print "mov " + item.out[0].value + ", " + target.out[0].reg_expect.name + "\n"
+print "mov " + item.out[1].value + ", " + target.out[1].reg_expect.name + "\n"
+
+print "/* do test */ \n"
+print spec.ins() + "\n"
+
+print "/* get test results*/\n"
+print "cmp " + target.out[0].reg_output.name + ", " + target.out[0].reg_expect.name + "\n"
+print "bne test_fail\n"
+
+print "cmp " + target.out[1].reg_output.name + ", " + target.out[1].reg_expect.name + "\n"
+print "bne test_fail\n"
+
+
+
+print "/* check results*/\n"

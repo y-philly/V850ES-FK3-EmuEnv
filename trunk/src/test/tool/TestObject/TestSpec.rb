54a16d60516192721b3f1f45fa3d95f63f1e9c55
takashi mori <tmori@esm.co.jp>
2017-01-07 10:44:00 +0900

    1項目消化した．

--- trunk/src/test/tool/TestObject/TestSpec.rb
+++ trunk/src/test/tool/TestObject/TestSpec.rb
@@ -51,11 +51,14 @@ class TestSpec
 
   def check_ins(item)
     check_instructions = Array.new()
+    check_instructions[check_instructions.length] = "/* get test results*/"
     target.out.each { |elm|
-      check_instructions[check_instructions.length] = "/* get test results*/"
       if elm.test_output.instance_of?(SystemRegister)
-        check_instructions[check_instructions.length] =  "ldsr " + elm.reg_output.name + ", " + elm.test_output.name
+        check_instructions[check_instructions.length] =  "stsr " + elm.test_output.name + ", " + elm.reg_output.name
       end
+    }
+    check_instructions[check_instructions.length] = "/* check test results*/"
+    target.out.each { |elm|
       check_instructions[check_instructions.length] =  "cmp " + elm.reg_output.name + ", " + elm.reg_expect.name
       check_instructions[check_instructions.length] =  "bne "+ item.name + "_test_fail"
     }

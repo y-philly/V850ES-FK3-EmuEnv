7553779f4c0cf49d919456a16eca1783111d4788
tmori <tmori@esm.co.jp>
2017-01-24 17:33:31 +0900

    ファイル名を変更し，引数チェックを追加した．

--- trunk/src/test/tool/generate_testcode.sh
+++ trunk/src/test/tool/generate_testcode.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+if [ $# -ne 2 ]
+then
+	echo "Usage: $0 test_item template.csv"
+	exit 1
+fi
+
 IFLAGS="-I ./TestObject -I ./TestObject/target -I ./TestObject/item -I ./TestObjectFactory -I ./TestCodeGenerator -I ./util"
+
 
 ruby ${IFLAGS} TestCodeGenerator/TestCodeGenerator.rb ${1} ${2}
 

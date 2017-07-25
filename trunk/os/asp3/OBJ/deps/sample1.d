5ebfac7f92300a59657fa62d3c2568db45815658
tmori <tmori@esm.co.jp>
2017-07-25 15:08:19 +0900

    ASP3をv850に移植するためのコミット

---
+++ trunk/os/asp3/OBJ/deps/sample1.d
@@ -0,0 +1,76 @@
+sample1.o: ../sample/sample1.c ../include/kernel.h ../include/t_stddef.h \
+ ../target/gr_peach_gcc/target_stddef.h \
+ c:\kpit\gnuarm-rzv14.01-eabi\arm-rz-eabi\arm-rz-eabi\lib\gcc\arm-rz-eabi\4.8-gnuarm-rz_v14.01\include\stdint.h \
+ c:\kpit\gnuarm-rzv14.01-eabi\arm-rz-eabi\arm-rz-eabi\arm-rz-eabi\include\stdint.h \
+ c:\kpit\gnuarm-rzv14.01-eabi\arm-rz-eabi\arm-rz-eabi\arm-rz-eabi\include\machine\_default_types.h \
+ c:\kpit\gnuarm-rzv14.01-eabi\arm-rz-eabi\arm-rz-eabi\arm-rz-eabi\include\sys\features.h \
+ ../arch/gcc/tool_stddef.h \
+ c:\kpit\gnuarm-rzv14.01-eabi\arm-rz-eabi\arm-rz-eabi\lib\gcc\arm-rz-eabi\4.8-gnuarm-rz_v14.01\include\stddef.h \
+ c:\kpit\gnuarm-rzv14.01-eabi\arm-rz-eabi\arm-rz-eabi\lib\gcc\arm-rz-eabi\4.8-gnuarm-rz_v14.01\include-fixed\limits.h \
+ ../arch/arm_gcc/rza1/chip_stddef.h ../arch/arm_gcc/common/core_stddef.h \
+ ../target/gr_peach_gcc/target_kernel.h \
+ ../arch/arm_gcc/rza1/chip_kernel.h ../arch/arm_gcc/common/core_kernel.h \
+ ../include/t_syslog.h ../include/t_stdlib.h ../syssvc/serial.h \
+ ../syssvc/syslog.h kernel_cfg.h ../sample/sample1.h \
+ ../target/gr_peach_gcc/target_test.h ../arch/arm_gcc/rza1/rza1.h \
+ ../include/sil.h ../target/gr_peach_gcc/target_sil.h \
+ ../arch/arm_gcc/common/core_sil.h ../arch/arm_gcc/common/core_test.h \
+ ../arch/arm_gcc/common/arm.h ../arch/arm_gcc/common/arm_insn.h
+
+../include/kernel.h:
+
+../include/t_stddef.h:
+
+../target/gr_peach_gcc/target_stddef.h:
+
+c:\kpit\gnuarm-rzv14.01-eabi\arm-rz-eabi\arm-rz-eabi\lib\gcc\arm-rz-eabi\4.8-gnuarm-rz_v14.01\include\stdint.h:
+
+c:\kpit\gnuarm-rzv14.01-eabi\arm-rz-eabi\arm-rz-eabi\arm-rz-eabi\include\stdint.h:
+
+c:\kpit\gnuarm-rzv14.01-eabi\arm-rz-eabi\arm-rz-eabi\arm-rz-eabi\include\machine\_default_types.h:
+
+c:\kpit\gnuarm-rzv14.01-eabi\arm-rz-eabi\arm-rz-eabi\arm-rz-eabi\include\sys\features.h:
+
+../arch/gcc/tool_stddef.h:
+
+c:\kpit\gnuarm-rzv14.01-eabi\arm-rz-eabi\arm-rz-eabi\lib\gcc\arm-rz-eabi\4.8-gnuarm-rz_v14.01\include\stddef.h:
+
+c:\kpit\gnuarm-rzv14.01-eabi\arm-rz-eabi\arm-rz-eabi\lib\gcc\arm-rz-eabi\4.8-gnuarm-rz_v14.01\include-fixed\limits.h:
+
+../arch/arm_gcc/rza1/chip_stddef.h:
+
+../arch/arm_gcc/common/core_stddef.h:
+
+../target/gr_peach_gcc/target_kernel.h:
+
+../arch/arm_gcc/rza1/chip_kernel.h:
+
+../arch/arm_gcc/common/core_kernel.h:
+
+../include/t_syslog.h:
+
+../include/t_stdlib.h:
+
+../syssvc/serial.h:
+
+../syssvc/syslog.h:
+
+kernel_cfg.h:
+
+../sample/sample1.h:
+
+../target/gr_peach_gcc/target_test.h:
+
+../arch/arm_gcc/rza1/rza1.h:
+
+../include/sil.h:
+
+../target/gr_peach_gcc/target_sil.h:
+
+../arch/arm_gcc/common/core_sil.h:
+
+../arch/arm_gcc/common/core_test.h:
+
+../arch/arm_gcc/common/arm.h:
+
+../arch/arm_gcc/common/arm_insn.h:
---
+++ trunk/os/asp3/OBJ/gen/tPutLogGRPeach_factory.h
@@ -0,0 +1,3 @@
+#ifndef tPutLogGRPeach_FACTORY_H
+#define tPutLogGRPeach_FACTORY_H
+#endif /* tPutLogGRPeach_FACTORY_H */
